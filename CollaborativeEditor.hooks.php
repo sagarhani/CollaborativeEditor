010b03bc90b2385ec8781c082ca43101d0934d3c
Sagar Hani <sagarhani33@gmail.com>
2016-06-19 00:21:34 +0530

    added hook

--- CollaborativeEditor.hooks.php
+++ CollaborativeEditor.hooks.php
@@ -8,17 +8,9 @@
 
 class CollaborativeEditorHooks {
 
-	public static function onParserFirstCallInit( Parser &$parser ) {
-		$parser->setFunctionHook( 'something', 'CollaborativeEditorHooks::doSomething' );
-	}
+	static function getModules( $out, $skin ) {
+		$out->addModules( array( 'ext.collaborativeEditor' ) );
 
-	public static function doSomething( Parser &$parser )
-	{
-		// Called in MW text like this: {{#something: }}
-
-		// For named parameters like {{#something: foo=bar | apple=orange | banana }}
-		// See: https://www.mediawiki.org/wiki/Manual:Parser_functions#Named_parameters
-
-		return "This text will be shown when calling this in MW text.";
+		return true;
 	}
 }

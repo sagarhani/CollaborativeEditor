aff4c8ff409bd8141e08e25268d967f2349406c1
Sagar Hani <sagarhani33@gmail.com>
2016-06-20 13:32:13 +0530

    Added hooks

--- CollaborativeEditor.php
+++ CollaborativeEditor.php
@@ -1,16 +1,31 @@
 <?php
 
-if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'CollaborativeEditor' );
-	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	$wgMessagesDirs['CollaborativeEditor'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['CollaborativeEditorAlias'] = __DIR__ . '/CollaborativeEditor.i18n.alias.php';
-	$wgExtensionMessagesFiles['CollaborativeEditorMagic'] = __DIR__ . '/CollaborativeEditor.i18n.magic.php';
-	wfWarn(
-		'Deprecated PHP entry point used for CollaborativeEditor extension. Please use wfLoadExtension ' .
-		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);
-	return true;
-} else {
-	die( 'This version of the CollaborativeEditor extension requires MediaWiki 1.25+' );
-}
+$moduleInfo = array(
+	'localBasePath' => __DIR__,
+	'remoteExtPath' => 'CollaborativeEditor',
+);
+
+$wgExtensionMessagesFiles['CollaborativeEditor'] = __DIR__ . '/CollaborativeEditor.i18n.php';
+
+$wgResourceModules['collaborativeEditor'] = array_merge( array(
+	'scripts' => array(
+		'modules/ext.collaborativeEditor.config.js',
+	),
+	'dependencies' => array(
+		'mediawiki.user',
+	),
+), $moduleInfo );
+
+$wgResourceModules['ext.collaborativeEditor'] = array_merge( array(
+	'scripts' => array(
+		'modules/ext.collaborativeEditor.js',
+		'modules/ext.collaborativeEditor.ve.js',
+	),
+
+	'dependencies' => array(
+		'collaborativeeditor',
+	),
+), $moduleInfo );
+
+$wgAutoloadClasses['CollaborativeEditorHooks'] = __DIR__ . '/CollaborativeEditor.hooks.php';
+$wgHooks['BeforePageDisplay'][] = 'CollaborativeEditorHooks::getModules';
